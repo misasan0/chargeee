f9301b59dc05e8043689d9d62c9a0a11cbba98a0
misasan0 <esat7274@gmail.com>
2025-04-22 19:51:36 +0300

    Update loading.tsx

--- app/admin/loading.tsx
+++ app/admin/loading.tsx
@@ -1,9 +1,7 @@
-import { Loader2 } from "lucide-react"
-
 export default function Loading() {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <Loader2 className="h-10 w-10 animate-spin text-primary" />
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-primary border-t-transparent"></div>
     </div>
   )
 }
